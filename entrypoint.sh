1ad17871e3d943ba47642b89bff14aa460499ab0
lijiao <lijiao@localhost.localdomain>
2015-08-07 19:45:50 +0800

    add sshd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+passwd <<EOF
+${ROOTPASS}
+${ROOTPASS}
+EOF
 
+/usr/sbin/sshd-keygen
+/usr/sbin/sshd -E /root/sshd_log  -f /root/sshd_config -p 22
+SSHPROXYADDR=172.17.0.1
+SSHPROXYPORT=22
+SSHPROXYPASS=dd13427
 func_cmd_need_password(){
    local password=$1
    shift 1
    expect -c "
        spawn $*
        expect {
-           \"*password:\" {set timeout 300; send \"${password}\r\";}
            \"*yes/no\" {send \"yes\r\"; exp_continue;}
+           \"*password:\" {set timeout 300; send \"${password}\r\";}
        }
    expect eof"
 }
@@ -16,5 +25,6 @@ CMD="ssh -p ${SSHPROXYPORT} -C -f -N -g -D 1111 root@${SSHPROXYADDR}"
 
 func_cmd_need_password "${SSHPROXYPASS}" "${CMD}"
 
-while true;do sleep 10; done
+while true;do sleep 10;  done
+
 
